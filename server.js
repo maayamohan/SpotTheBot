36b6cb7f3644deff1693f0d5e5663a0b82c39d93
Maaya Mohan <maayamohan21@gmail.com>
2025-11-20 04:45:41 +0530

    feat: Added server.js with room architecture

---
+++ server.js
@@ -0,0 +1,91 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+    res.send("WebSocket Server is Running");
+});
+
+const wss = new WebSocketServer({ server });
+
+const rooms = new Map();
+
+wss.on("connection", (ws) => {
+    console.log("Client Connected");
+    ws.room = null;
+
+    ws.on("message", (data) => {
+        // console.log("Received: ", data.toString());
+        let msg;
+
+        try {
+            msg = JSON.parse(data.toString());
+        }
+        catch (e) {
+            console.log("Invalid JSON: ", data.toString);
+            return;
+        }
+
+        if (msg.type == "join-room") {
+            const roomCode = msg.room;
+
+            if (!rooms.has(roomCode)) {
+                rooms.set(roomCode, new Set());
+            }
+
+            rooms.get(roomCode).add(ws);
+            ws.room = roomCode;
+
+            console.log(`Client joined room ${roomCode}`);
+
+            broadcastToRoom(roomCode, {
+                type: "room-update",
+                message: "A new player has joined!",
+                count: rooms.get(roomCode).size
+            });
+        }
+
+        ws.send("Server received: " + data.toString());
+    });
+
+    ws.on("close", () => {
+        // console.log("Client Disconnected");
+        if (ws.room) {
+            const roomSet = rooms.get(ws.room);
+            if (roomSet) {
+                roomSet.delete(ws);
+
+                broadcastToRoom(ws.room, {
+                    type: "room-update",
+                    message: "A player has left!",
+                    count: rooms.get(ws.room).size
+                });
+
+                if (roomSet.size === 0) {
+                    rooms.delete(ws.room);
+                }
+            }
+
+            console.log(`Client has left room ${ws.room}`);
+        }
+    });
+});
+
+function broadcastToRoom(room, obj) {
+    const roomSet =rooms.get(room);
+    if (!roomSet) {
+        return;
+    }
+    const json = JSON.stringify(obj);
+
+    for (const client of roomSet) {
+        client.send(json);
+    }
+}
+
+server.listen(3000, () => {
+    console.log("Server listening on http://localhost:3000");
+});
